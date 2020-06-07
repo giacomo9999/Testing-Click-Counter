d38793f7fcbe10497f1ea63bffa1d861d4a5ca27
giacomo9999 <gmarchesi888@gmail.com>
2020-06-07 15:48:55 -0400

    Added testing dependencies.

--- src/App.js
+++ src/App.js
@@ -10,16 +10,18 @@ class App extends Component {
   }
   render() {
     return (
-      <div data-test="component-app">
-        <h1 data-test="counter-display">
-          The counter is currently {this.state.counter}
-        </h1>
-        <button
-          data-test="increment-button"
-          onClick={() => this.setState({ counter: this.state.counter + 1 })}
-        >
-          Increment counter
-        </button>
+      <div className="container-outer">
+        <div className="container-inner" data-test="component-app">
+          <h1 data-test="counter-display">
+            The counter is currently {this.state.counter}
+          </h1>
+          <button
+            data-test="increment-button"
+            onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          >
+            Increment counter
+          </button>
+        </div>
       </div>
     );
   }
