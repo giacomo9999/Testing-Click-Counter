a5c1034959ce51d4749259491136db625df2a93f
giacomo9999 <gmarchesi888@gmail.com>
2020-06-07 16:44:32 -0400

    Beginning to write tests.

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,19 @@ import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders increment button", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='increment-button']");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders counter display", () => {});
+test("counter starts at 0", () => {});
+test("clicking button increments counter display", () => {});
